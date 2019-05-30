748d5bb56ed205a84bb924aa2cead23f2b4ef7e7
dstoiano <dima>
2019-05-30 17:40:10 +0300

    upd

--- src/gt/plot/regulation/Mortality1YearMixedNashPlot.py
+++ src/gt/plot/regulation/Mortality1YearMixedNashPlot.py
@@ -1,21 +1,23 @@
-from gt.plot.HospitalNashPlot import PureNashPlot
+from gt.plot.HospitalNashMixedPlot import NashMixedPlot
 from gt.core.HospitalWith1YModel import HospitalWith1YM
 from common import *
 
-class M1YNashPlot(PureNashPlot):
+
+class M1YMixedNashPLot(NashMixedPlot):
+
     def find_solution(self, l, mu, n):
-        hospital = HospitalWith1YM(l, mu, n, N_lim, t_c, 5, cost_transp + cost_op)
+        hospital = HospitalWith1YM(l, mu, n, N_lim, t_c, cost_op, cost_op + cost_transp)
         G = hospital.game_matrix()
         A = self.extract_player_utility(G, 0)
         B = self.extract_player_utility(G, 1)
         game = nash.Game(A, B)
         eqs = list(game.support_enumeration())
         if self.is_system_consistent(A, B, eqs):
-            sol = self.skip_mixed_strategy(eqs)
+            sol = self.mixed_or_pure_solution_value(eqs)
             return sol
         else:
             return None
 
 
 if __name__ == '__main__':
-    M1YNashPlot().show('../../../images/regulation/Hospital with M1Y regulation Mixed Nash Equ low revenue')
+    M1YMixedNashPLot().show('../../../images/regulation/Hospital with 1YM regulation Mixed Nash Equ low revenue')
--- src/gt/plot/regulation/Mortality1YearNashPlot.py
+++ src/gt/plot/regulation/Mortality1YearNashPlot.py
@@ -1,23 +1,21 @@
-from gt.plot.HospitalNashMixedPlot import NashMixedPlot
+from gt.plot.HospitalNashPlot import PureNashPlot
 from gt.core.HospitalWith1YModel import HospitalWith1YM
 from common import *
 
-
-class M1YMixedNashPLot(NashMixedPlot):
-
+class M1YNashPlot(PureNashPlot):
     def find_solution(self, l, mu, n):
-        hospital = HospitalWith1YM(l, mu, n, N_lim, t_c, cost_op, cost_op + cost_transp)
+        hospital = HospitalWith1YM(l, mu, n, N_lim, t_c, 5, cost_transp + cost_op)
         G = hospital.game_matrix()
         A = self.extract_player_utility(G, 0)
         B = self.extract_player_utility(G, 1)
         game = nash.Game(A, B)
         eqs = list(game.support_enumeration())
         if self.is_system_consistent(A, B, eqs):
-            sol = self.mixed_or_pure_solution_value(eqs)
+            sol = self.skip_mixed_strategy(eqs)
             return sol
         else:
             return None
 
 
 if __name__ == '__main__':
-    M1YMixedNashPLot().show('../../../images/regulation/Hospital with 1YM regulation Mixed Nash Equ low revenue')
+    M1YNashPlot().show('../../../images/regulation/Hospital with M1Y regulation Mixed Nash Equ low revenue')
