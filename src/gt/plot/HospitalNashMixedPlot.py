7b1b441fa4b1a1fd18981c8bcbb744ba23bda3ff
Dmitriy <demon007>
2019-06-14 00:31:01 +0300

    upd presentation

--- src/gt/plot/HospitalNashMixedPlot.py
+++ src/gt/plot/HospitalNashMixedPlot.py
@@ -10,10 +10,10 @@ class NashMixedPlot:
     def mixed_or_pure_solution_value(eqs):
         for eq in eqs:
             if eq[0][0] != 1 and eq[0][1] != 1:
-                return int(round(255 * eq[1][0])), 0, int(round(eq[0][0] * 255))
+                return int(round(191 * eq[1][0])) + 64, 0, int(round(eq[0][0] * 191)) + 64
         if len(eqs) == 1:
             eq = eqs[0]
-            return int(round(eq[1][0] * 255)), 0, int(round(255 * eq[0][0]))
+            return int(round(eq[1][0] * 191)) + 64, 0, int(round(191 * eq[0][0])) + 64
 
     @staticmethod
     def is_system_consistent(A, B, eqs):
@@ -42,11 +42,12 @@ class NashMixedPlot:
         data = []
         points = 20
         x_ticks = [round(x, 2) for x in np.linspace(la_min, la_max, points)]
-        y_ticks = [round(x, 2) for x in np.linspace(la_min, la_max, points)]
-        for l in x_ticks:
+        y_ticks = [round(x, 2) for x in np.linspace(mu_min, mu_max, points)]
+        for mu in y_ticks:
             data.append([])
-            for mu in y_ticks:
-                data[-1].append(self.find_solution(l, mu, n))
+            for la in x_ticks:
+                sol = self.find_solution(la, mu, n)
+                data[-1].append(sol)
         plt.sca(ax)
         plt.imshow(data, extent=[la_min, la_max, mu_min, mu_max])
         ax.set_title('Nash Mixed Equ N = ' + str(n), fontdict={'fontsize': 22})
@@ -55,8 +56,8 @@ class NashMixedPlot:
         ax.set_xticklabels(x_ticks)
         ax.set_xlabel('Lambda', fontdict={'fontsize': 20})
         ax.set_ylabel('Mu', fontdict={'fontsize': 20})
-        ax.set_yticks(np.linspace(mu_max, mu_min, 10))
-        ax.set_yticklabels(x_ticks)
+        ax.set_yticks(np.linspace(mu_max,mu_min, 10))
+        ax.set_yticklabels(y_ticks)
 
     def color_bar_2d(self, ax):
         plt.sca(ax)
@@ -64,20 +65,20 @@ class NashMixedPlot:
         for r in np.linspace(0, 1, 100):
             data.append([])
             for b in np.linspace(0, 1, 100):
-                data[-1].append((r, 0, b))
-        plt.imshow(data, extent=[0, 255, 0, 255])
+                data[-1].append(((int(r*191)+64), 0, (int(191*b)+64)))
+        plt.imshow(data, extent=[64, 255, 64, 255])
         ax = plt.gca()
         ax.invert_yaxis()
         ax.set_title('Strategies Color Map', fontdict={'fontsize': 25})
-        ax.set_xticks([0, 255])
+        ax.set_xticks([64, 255])
         ax.set_xticklabels(['R', 'A'])
         ax.set_xlabel('Hospital 1 Strategy', fontdict={'fontsize': 20})
-        ax.set_yticks([0, 255])
+        ax.set_yticks([64, 255])
         ax.set_yticklabels(['A', 'R'])
         ax.set_ylabel('Hospital 2 Strategy', fontdict={'fontsize': 20})
 
     def show(self, filename='../../images/previous/Hospital Mixed Nash Equ'):
-        fig = plt.figure(constrained_layout=True, figsize=(23, 23))
+        fig = plt.figure(constrained_layout=True, figsize=(25, 25))
         gs = fig.add_gridspec(3, 4)
         self.color_bar_2d(fig.add_subplot(gs[:, 3]))
         self.solution_plot([1, 1], ax=fig.add_subplot(gs[0, 0]))
